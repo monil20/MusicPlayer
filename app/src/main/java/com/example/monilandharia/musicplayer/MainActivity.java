bb36921c6d9dace2fc6b013d7e4cf6aa2c04d1c7
monil.andharia <monil20>
2018-05-30 12:43:47 +0530

    #4- All animations done

--- app/src/main/java/com/example/monilandharia/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/monilandharia/musicplayer/MainActivity.java
@@ -4,12 +4,18 @@ import android.animation.ObjectAnimator;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.graphics.Color;
 import android.graphics.Path;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
@@ -23,10 +29,13 @@ public class MainActivity extends AppCompatActivity {
     private SlidingUpPanelLayout mLayout;
     private CardView cardView;
     private PlayPauseView playPauseView;
-    private TextView trackName;
+    private TextView trackName, album_artist, startTime, endTime;
     //    private NowPlayingFragment nowPlayingFragment;
-    private float playPauseOriX, playPauseOriY, trackX, trackY, x, y;
+    private float playPauseOriX, playPauseOriY, trackX, trackY, albumArtistX, albumArtistY, x, y, tempY;
     private AHBottomNavigation bottomNavigation;
+    private ImageView next, prev, repeat, shuffle;
+    private boolean flag = true, flag1 = false;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -121,8 +130,14 @@ public class MainActivity extends AppCompatActivity {
                 playPauseView.setY(-30);
                 trackX = trackName.getX();
                 trackY = trackName.getY();
-                trackName.setX(30);
+                trackName.setX(250);
                 trackName.setY(30);
+                albumArtistX = album_artist.getX();
+                albumArtistY = album_artist.getY();
+                album_artist.setX(250);
+                tempY = y;
+                album_artist.setY(y + trackName.getHeight() + 26);
+                flag1 = true;
             }
         });
 
@@ -130,32 +145,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPanelSlide(View panel, float slideOffset) {
                 Log.i("Slide", "onPanelSlide, offset " + slideOffset);
-                if (slideOffset >= 0 && slideOffset <= 0.1) {
-                    cardView.setAlpha(0);
-                } else if (slideOffset > 0.1 && slideOffset <= 0.2) {
-                    cardView.setAlpha(0.1f);
-                } else if (slideOffset > 0.2 && slideOffset <= 0.3) {
-                    cardView.setAlpha(0.2f);
-                } else if (slideOffset > 0.3 && slideOffset <= 0.4) {
-                    cardView.setAlpha(0.3f);
-                } else if (slideOffset > 0.4 && slideOffset <= 0.5) {
-                    cardView.setAlpha(0.4f);
-                } else if (slideOffset > 0.5 && slideOffset <= 0.6) {
-                    cardView.setAlpha(0.5f);
-                } else if (slideOffset > 0.6 && slideOffset <= 0.7) {
-                    cardView.setAlpha(0.6f);
-                } else if (slideOffset > 0.7 && slideOffset <= 0.8) {
-                    cardView.setAlpha(0.7f);
-                } else if (slideOffset > 0.8 && slideOffset <= 0.9) {
-                    cardView.setAlpha(0.8f);
-                } else if (slideOffset > 0.9 && slideOffset <= 1.0) {
-                    cardView.setAlpha(0.9f);
-                }
+//                if (slideOffset >= 0 && slideOffset <= 0.1) {
+//                    cardView.setAlpha(0);
+//                } else if (slideOffset > 0.1 && slideOffset <= 0.2) {
+//                    cardView.setAlpha(0.1f);
+//                } else if (slideOffset > 0.2 && slideOffset <= 0.3) {
+//                    cardView.setAlpha(0.2f);
+//                } else if (slideOffset > 0.3 && slideOffset <= 0.4) {
+//                    cardView.setAlpha(0.3f);
+//                } else if (slideOffset > 0.4 && slideOffset <= 0.5) {
+//                    cardView.setAlpha(0.4f);
+//                } else if (slideOffset > 0.5 && slideOffset <= 0.6) {
+//                    cardView.setAlpha(0.5f);
+//                } else if (slideOffset > 0.6 && slideOffset <= 0.7) {
+//                    cardView.setAlpha(0.6f);
+//                } else if (slideOffset > 0.7 && slideOffset <= 0.8) {
+//                    cardView.setAlpha(0.7f);
+//                } else if (slideOffset > 0.8 && slideOffset <= 0.9) {
+//                    cardView.setAlpha(0.8f);
+//                } else if (slideOffset > 0.9 && slideOffset <= 1.0) {
+//                    cardView.setAlpha(0.9f);
+//                }
             }
 
             @Override
             public void onPanelStateChanged(View panel, PanelState previousState, PanelState newState) {
                 Log.i("State", "onPanelStateChanged " + newState);
+                if (newState.toString().equals("DRAGGING")) {
+                    if (previousState.toString().equals("EXPANDED")) {
+                        flag = false;
+                    }
+                }
                 if (newState.toString().equals("EXPANDED")) {
                     x = playPauseView.getX();
                     y = playPauseView.getY();
@@ -165,7 +185,7 @@ public class MainActivity extends AppCompatActivity {
                     ObjectAnimator objectAnimator =
                             ObjectAnimator.ofFloat(playPauseView, View.X,
                                     View.Y, path);
-                    objectAnimator.setDuration(400);
+                    objectAnimator.setDuration(300);
 
                     Path path1 = new Path();
                     x = trackName.getX();
@@ -177,36 +197,106 @@ public class MainActivity extends AppCompatActivity {
                                     View.Y, path1);
                     objectAnimator1.setDuration(220);
 
-                    objectAnimator.start();
-                    objectAnimator1.start();
+                    Path path2 = new Path();
+                    x = album_artist.getX();
+                    y = album_artist.getY();
+                    path2.moveTo(x, y);
+                    path2.lineTo(albumArtistX, albumArtistY);
+                    ObjectAnimator objectAnimator2 =
+                            ObjectAnimator.ofFloat(album_artist, View.X,
+                                    View.Y, path2);
+                    objectAnimator2.setDuration(250);
+
+                    Animation animationFadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein);
+
+                    if (flag) {
+                        objectAnimator.start();
+                        objectAnimator1.start();
+                        objectAnimator2.start();
+                        prev.startAnimation(animationFadeIn);
+                        next.startAnimation(animationFadeIn);
+                        shuffle.startAnimation(animationFadeIn);
+                        repeat.startAnimation(animationFadeIn);
+                        cardView.startAnimation(animationFadeIn);
+                        seekBar.setAnimation(animationFadeIn);
+                        startTime.setAnimation(animationFadeIn);
+                        endTime.setAnimation(animationFadeIn);
+                        final Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                            }
+                        }, 900);
+                        repeat.setAlpha(1.0f);
+                        shuffle.setAlpha(1.0f);
+                        next.setAlpha(1.0f);
+                        prev.setAlpha(1.0f);
+                        cardView.setAlpha(1.0f);
+                        seekBar.setAlpha(1.0f);
+                        startTime.setAlpha(1.0f);
+                        endTime.setAlpha(1.0f);
+
+                    }
+
+                    flag = true;
+                    flag1 = false;
+
                 }
                 if (newState.toString().equals("COLLAPSED")) {
-                    x = playPauseView.getX();
-                    playPauseOriX = x;
-                    y = playPauseView.getY();
-                    playPauseOriY = y;
-                    Path path = new Path();
-                    path.moveTo(x, y);
-                    path.lineTo(x + x + 10, -30);
-                    ObjectAnimator objectAnimator =
-                            ObjectAnimator.ofFloat(playPauseView, View.X,
-                                    View.Y, path);
-                    objectAnimator.setDuration(0);
+                    if (!flag1) {
+                        x = playPauseView.getX();
+                        playPauseOriX = x;
+                        y = playPauseView.getY();
+                        playPauseOriY = y;
+                        Path path = new Path();
+                        path.moveTo(x, y);
+                        path.lineTo(x + x + 10, -30);
+                        ObjectAnimator objectAnimator =
+                                ObjectAnimator.ofFloat(playPauseView, View.X,
+                                        View.Y, path);
+                        objectAnimator.setDuration(0);
 
-                    x = trackName.getX();
-                    trackX = x;
-                    y = trackName.getY();
-                    trackY = y;
-                    Path path1 = new Path();
-                    path1.moveTo(x, y);
-                    path1.lineTo(30, 30);
-                    ObjectAnimator objectAnimator1 =
-                            ObjectAnimator.ofFloat(trackName, View.X,
-                                    View.Y, path1);
-                    objectAnimator1.setDuration(0);
+                        Log.i("height", playPauseView.getHeight() + "");
 
-                    objectAnimator.start();
-                    objectAnimator1.start();
+                        x = trackName.getX();
+                        trackX = x;
+                        y = trackName.getY();
+                        trackY = y;
+                        Path path1 = new Path();
+                        path1.moveTo(x, y);
+                        path1.lineTo(250, 30);
+                        ObjectAnimator objectAnimator1 =
+                                ObjectAnimator.ofFloat(trackName, View.X,
+                                        View.Y, path1);
+                        objectAnimator1.setDuration(0);
+
+                        x = album_artist.getX();
+                        albumArtistX = x;
+                        y = album_artist.getY();
+                        albumArtistY = y;
+                        Path path2 = new Path();
+                        path2.moveTo(x, y);
+                        path2.lineTo(250, tempY + trackName.getHeight() + 26);
+                        ObjectAnimator objectAnimator2 =
+                                ObjectAnimator.ofFloat(album_artist, View.X,
+                                        View.Y, path2);
+                        objectAnimator2.setDuration(0);
+
+                        objectAnimator.start();
+                        objectAnimator1.start();
+                        objectAnimator2.start();
+                        next.setAlpha(0.0f);
+                        prev.setAlpha(0.0f);
+                        repeat.setAlpha(0.0f);
+                        shuffle.setAlpha(0.0f);
+                        cardView.setAlpha(0.0f);
+                        seekBar.setAlpha(0.0f);
+                        startTime.setAlpha(0.0f);
+                        endTime.setAlpha(0.0f);
+
+                        flag = true;
+                        flag1 = true;
+                    }
                 }
             }
         });
@@ -226,8 +316,15 @@ public class MainActivity extends AppCompatActivity {
         playPauseView = findViewById(R.id.play_pause);
         trackName = findViewById(R.id.album_track);
         bottomNavigation = findViewById(R.id.bottom_navigation);
+        album_artist = findViewById(R.id.album_artist_name);
+        next = findViewById(R.id.next);
+        prev = findViewById(R.id.prev);
+        repeat = findViewById(R.id.repeat);
+        shuffle = findViewById(R.id.shuffle);
+        seekBar = findViewById(R.id.seekBar);
+        startTime = findViewById(R.id.start_time);
+        endTime = findViewById(R.id.end_time);
     }
-
 
     @Override
     public void onBackPressed() {
